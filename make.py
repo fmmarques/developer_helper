39248f4aaefecdb8512702057853e76279fffbd6
Frederico Marques <1880864+fmmarques@users.noreply.github.com>
2024-07-26 22:57:50 +0100

    Modify make to create the commands

--- make.py
+++ make.py
@@ -5,23 +5,57 @@ import click
 import os
 from abc import ABC, abstractmethod
 
-class Generator:
-    def __init__(self, name,  build_dir, source_dir):
-        self.name = name
-        self.build_dir = build_dir
-        self.source_dir = source_dir
-        self.generate_command = None
-        self.build_command = None
+class Project:
+    def __init__(self):
+        self.generate = None
 
     def __str__(self):
-        return f'build_dir: {self.build_dir}' \
-               f'source_dir: {self.source_dir}'
+        return f'project generate: {self.generate}'
+
+class Build:
+    def __init__(self):
+        self.all = None
+        self.clean = None
+        self.rebuild = None
+
+    def __str__(self):
+        return  f'build all: {self.all}'\
+                f'build clean: {self.clean}'\
+                f'build rebuild: {self.rebuild}'
+class Test:
+    def __init__(self):
+        self.all = None
+
+    def __str__(self):
+        return  f'test all: {self.all}'
+
+class Generator:
+    def __init__(self, name, source_directory, build_directory):
+        self.name = name
+        self.build_directory = build_directory
+        self.source_directory = source_directory
+        self.project = Project()
+        self.build = Build()
+        self.test = Test()
+
+    def __str__(self):
+        return  f'name : {self.name}'\
+                f'source_dir: {self.source_directory}'\
+                f'build_directory: {self.build_directory}'\
+                f'project: {self.project}'\
+                f'build: {self.build}'\
+                f'test: {self.test}'
+
+
 
 class Builder:
-    def format_generate_command(self):
+    def construct_project(self):
         pass
 
-    def format_build_command(self):
+    def construct_build(self):
+        pass
+
+    def construct_test(self):
         pass
 
     def get_generator(self):
@@ -29,29 +63,41 @@ class Builder:
 
 
 class MakeGeneratorBuilder(Builder):
-    def __init__(self, build_dir="./build/make", source_dir='./'):
-        self.generator = Generator("Unix Makefiles", build_dir, source_dir)
+    def __init__(self,  source_directory='./', build_directory="./build/make"):
+        self.generator = Generator("Unix Makefiles", source_directory, build_directory)
 
-    def format_generator_command(self):
-        self.generator.generate_command = f'cmake -G"{self.generator.name}" -B"{self.generator.build_dir}" -S"{self.generator.source_dir}"'
+    def construct_project(self):
+        self.generator.project.generate = f'cmake -G"{self.generator.name}" -B"{self.generator.build_directory}" -S"{self.generator.source_directory}"'
 
-    def format_build_command(self):
-        self.generator.build_command = f"make -C '{self.generator_build_dir}/' all"
+    def construct_build(self):
+        self.generator.build.all = f"make -C '{self.generator.build_directory}/' all"
+        self.generator.build.clean = f"make -C '{self.generator.build_directory}/' clean"
+        self.generator.build.rebuild = self.generator.build.clean + " ; " + self.generator.build.all
+
+    def construct_test(self):
+        self.generator.test.all = f"make -C '{self.generator.build_directory}' test"
 
     def get_generator(self):
         return self.generator
 
 class VisualStudioGeneratorBuilder(Builder):
-    def __init__(self, version=16, year=2022, build_dir="./build/vs", source_dir='./'):
-        self.generator = Generator("Visual Studio {version} {year}", build_dir, source_dir)
+    def __init__(self, source_directory='./', build_directory="./build/vs", version=16, year=2022 ):
+        self.generator = Generator("Visual Studio {version} {year}", build_directory=build_directory, source_directory=source_directory)
         self.generator.version = version
         self.generator.year = year
 
-    def format_generator_command(self):
-        self.generator.generate_command = f'cmake -G"{self.generator.name}" -B"{self.generator.build_dir}" -S"{self.generator.source_dir}"'
+    def construct_project(self):
+        self.generator.project.generate = f'cmake -G"{self.generator.name}" -B"{self.generator.build_directory}" -S"{self.generator.source_directory}"'
 
-    def format_build_command(self):
-        self.generator.build_command = 'Part 2'
+    def construct_build(self):
+        raise RuntimeError("Not implemented")
+        self.generator.build.all = f"msbuild "
+        self.generator.build.clean = f"msbuild"
+        self.generator.build.rebuild = f"msbuild"
+
+    def construct_test(self):
+        raise RuntimeError("Not implemented")
+        self.generator.test.all = f"make -C '{self.generator.build_directory}' test"
 
     def get_generator(self):
         return self.generator
@@ -62,74 +108,104 @@ class GeneratorBuilder(ABC):
     def create_generator(self):
         pass
 
-    def get_generator(self):
-        generator = self.create_generator()
-        generator.format_generator_command()
-        generator.format_build_command()
+    def get_generator(self, *kargs, **kwargs):
+        generator = self.create_generator(*kargs, **kwargs)
+        generator.construct_project()
+        generator.construct_build()
+        generator.construct_test()
         return generator.get_generator()
 
 # Concrete Creators
+class DefaultGeneratorCreator(GeneratorBuilder):
+    def create_generator(self, source_directory="./", build_directory="./build/"):
+        return MakeGeneratorBuilder(source_directory, build_directory)
+
+# Concrete Creators
 class MakeGeneratorCreator(GeneratorBuilder):
-    def create_generator(self, build_directory="./build/"):
-        return MakeGeneratorBuilder()
+    def create_generator(self, source_directory="./", build_directory="./build/"):
+        return MakeGeneratorBuilder(source_directory, build_directory)
 
 class VisualStudioGeneratorCreator(GeneratorBuilder):
-    def create_generator(self):
-        return VisualStudioGeneratorBuilder()
+    def create_generator(self, source_directory="./", build_directory="./build/", version=16, year=2022):
+        return VisualStudioGeneratorBuilder(source_directory, build_directory, version, year)
 
-def make_generator_from(generator_creator_cls, *args, **kwargs):
+def make_generator_from_creator_class(generator_creator_cls, *args, **kwargs):
     try:
         # Get the class object by name
         cls = globals()[generator_creator_cls]
 
         # Instantiate the class with any additional arguments
-        instance = cls(*args, **kwargs)
-
+        instance = cls()
         # Return the generator
-        return instance.get_generator()
+        return instance.get_generator(*args, **kwargs)
 
     except KeyError:
         print(f"Class '{generator_creator_cls}' not found.")
     except Exception as e:
         print(f"Error instantiating class '{generator_creator_cls}': {e}")
 
-def do_command(cmd, debug=False):
-    if (debug):
+def do_command(cmd, dry_run=False):
+    if (dry_run):
         click.echo(cmd)
+        return
     process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
     for line in iter(process.stdout.readline, b''):
         print(line.decode('utf-8').strip())
     process.communicate()
 
 
-@click.group(help="Project related commands. Use \"./make.py project <SUBCOMMAND>\" to learn about the supported subcommand.")
+@click.group(help="Use \"./make.py <COMMAND>\" for more information")
+def cli():
+    pass
+
+@cli.group(help="Project related commands. Use \"./make.py project <SUBCOMMAND>\" to learn about the supported subcommand.")
 def project():
     pass
 
-@click.command(help="Creates default project onto \"./build\" directory.")
-def make():
-    generator_creator = make_generator_from(generator_cls="MakeGeneratorCreator", )
-    do_command(generator.generate_command, debug=True)
+@project.command(help="Creates the project onto \"./build\" directory.")
+@click.option('--project_type', default="make", type=str, help="")
+@click.option('--source_directory', default=".", type=str, help="The directory where the project source is (containing CMakeLists.txt)")
+@click.option('--build_directory', default="./build", type=str, help="The directory where the project will be built")
+@click.option('--dry_run', default=False, type=bool, help="Echo the cmake command")
+def generate(project_type, source_directory, build_directory, dry_run):
+    cls = "DefaultGeneratorCreator"
+    if (project_type == "make"):
+        cls = "MakeGeneratorCreator"
+    elif (project_type == "vs" or project_type == "vsts" or project_type == "visualstudio"):
+        cls = "VisualStudioGeneratorCreator"
+    generator = make_generator_from_creator_class(cls, build_directory=build_directory, source_directory=source_directory)
+    do_command(generator.project.generate, dry_run=dry_run)
 
-@click.command(help="Creates Visual Studio project onto \"./build\" directory.")
-@click.option('--version', default=17, type=int, help="The desired version of Visual Studio")
-@click.option('--year', default=2022, type=int, help="The year of Visual Studio version")
-def visual_studio(version, year):
-    do_command(f"cmake -B './build/visual-studio-{version}-{year}' -G'Visual Studio {version} {year}' .")
-    do_command(f"echo 'visual-studio-{version}-{year}' > ./build/current-vsst")
+def make_generator_from_filesystem(source_directory, build_directory):
+    def make_makegenerator_from_filesystem(source_directory, build_directory):
+        def does_makefile_exists(build_directory):
+            return os.path.exists(f"{build_directory}/Makefile")
 
+        if (not does_makefile_exists(build_directory)):
+            raise RuntimeError(f"Couldn't find a Makefile on {build_directory}. Try running \"{__file__} project make" "source_directory {source_directory} --build_directory {build_directory}\"")
+        generator = make_generator_from_creator_class("MakeGeneratorCreator", build_directory=build_directory, source_directory=source_directory)
+        return generator
 
-@click.command(help="Refresh default project on \"./build\" directory.")
-def refresh_make():
-    do_command("cmake --fresh -B './build/make' -G'Unix Makefiles' .")
+    def make_vsgenerator_from_filesystem(source_directory, build_directory):
+        raise RuntimeError("Not implemented")
 
-project.add_command(make)
-project.add_command(refresh_make)
-project.add_command(visual_studio)
+    generator = None
+    try:
+        generator = make_makegenerator_from_filesystem(source_directory, build_directory)
+    except RuntimeError as re:
+        print(e)
+        pass # Expected to exception if not found.
+
+    try:
+        generator = make_vsgenerator_from_filesystem(source_directory, build_directory)
+    except RuntimeError as re:
+        pass # Expected to exception if not found.
+
+    if (generator is None):
+        raise RuntimeError(f"Couldn't find a generated project on the specified build_directory ({build_directory}).")
+    return generator
 
 def ensure_project_exists():
-    makefile_exists = os.path.exists("./build/make/Makefile")
-
     visualstudio_exists = False;
     if (os.path.exists("./build/current_vsst")):
         with open("./build/current_vsst", 'r') as file:
@@ -137,47 +213,46 @@ def ensure_project_exists():
         visualstudio_exists = os.path.exists(vsts_dir)
     return makefile_exists or visualstudio_exists
 
-@click.group(help="Build related commands. Use \"./make.py build\" to learn about the supported subcommands.")
+@cli.group(help="Build related commands. Use \"./make.py build\" to learn about the supported subcommands.")
 def build():
     pass
 
-@click.command()
-def all():
-    ensure_project_exists()
-    do_command("make -C './build/'")
+@build.command()
+@click.option('--source_directory', default=".", type=str, help="The directory where the project source is (containing CMakeLists.txt)")
+@click.option('--build_directory', default="./build", type=str, help="The directory where the project will be built")
+@click.option('--dry_run', default=False, type=bool, help="Echo the cmake command")
+def all(source_directory, build_directory, dry_run):
+    generator = make_generator_from_filesystem(source_directory, build_directory)
+    do_command(generator.build.all, dry_run=dry_run)
 
-@click.command()
-def clean():
-    ensure_makefile_exists()
-    do_command("make -C './build/' clean")
+@build.command()
+@click.option('--source_directory', default=".", type=str, help="The directory where the project source is (containing CMakeLists.txt)")
+@click.option('--build_directory', default="./build", type=str, help="The directory where the project will be built")
+@click.option('--dry_run', default=False, type=bool, help="Echo the cmake command")
+def clean(source_directory, build_directory, dry_run):
+    generator = make_generator_from_filesystem(source_directory, build_directory)
+    do_command(generator.build.clean, dry_run=dry_run)
 
-@click.command()
-def rebuild():
-    ensure_makefile_exists()
-    do_command("make -C './build/' clean")
-    do_command("make -C './build/' all")
+@build.command()
+@click.option('--source_directory', default=".", type=str, help="The directory where the project source is (containing CMakeLists.txt)")
+@click.option('--build_directory', default="./build", type=str, help="The directory where the project will be built")
+@click.option('--dry_run', default=False, type=bool, help="Echo the cmake command")
+def rebuild(source_directory, build_directory, dry_run):
+    generator = make_generator_from_filesystem(source_directory, build_directory)
+    do_command(generator.build.clean, dry_run=dry_run)
+    do_command(generator.build.all, dry_run=dry_run)
 
-build.add_command(all)
-build.add_command(clean)
-build.add_command(rebuild)
-
-@click.group(help="test related commands. Use \"./make.py test <SUBCOMMAND>\" to learn about the supported subcommands.")
+@cli.group(help="test related commands. Use \"./make.py test <SUBCOMMAND>\" to learn about the supported subcommands.")
 def test():
     pass
 
-@click.command()
-def runAll():
-    result = subprocess.run(['make', '-C', './build/', 'clean'], stdout=subprocess.PIPE)
-    print(result.stdout.decode('utf-8'))
-
-
-@click.group(help="Use \"./make.py <COMMAND>\" for more information")
-def verbs():
-    pass
-
-verbs.add_command(project)
-verbs.add_command(build)
-verbs.add_command(test)
+@test.command()
+@click.option('--source_directory', default=".", type=str, help="The directory where the project source is (containing CMakeLists.txt)")
+@click.option('--build_directory', default="./build", type=str, help="The directory where the project will be built")
+@click.option('--dry_run', default=False, type=bool, help="Echo the cmake command")
+def all(source_directory, build_directory, dry_run):
+    generator = make_generator_from_filesystem(source_directory, build_directory)
+    do_command(generator.test.all, dry_run=dry_run)
 
 if __name__ == '__main__':
-    verbs()
+    cli()
